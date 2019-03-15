c293a43a506d60b8a927bb5a4561d8b2310b2ac5
youssephAhmed <yousseffahmed@hotmail.com>
2019-03-15 02:02:15 +0200

    Youssef - changed 'TopModule.v' - added comments about block interfacing and fixing syntax error

--- TopModule.v
+++ TopModule.v
@@ -1,14 +1,26 @@
-`define NUMBER_OF_CLAUSES 3 // does not include "inside" constraint
-`define NUMBER_OF_INTEGER_VARIABLES 2
-`define NUMBER_OF_BOOLEAN_VARIABLES 2
-`define BIT_WIDTH_OF_INTEGER_VARIABLE 32
-`define CLAUSE_BIT_WIDTH ((NUMBER_OF_INTEGER_VARIABLES*BIT_WIDTH_OF_COEFFICIENTS)+(2*NUMBER_OF_BOOLEAN_VARIABLES))//(2*NUMBER_OF_BOOLEAN_VARIABLES) 1 bit to indicate whether is exist or not and the other bit is a the constraint (x or !x)
-`define BIT_WIDTH_OF_COEFFICIENTS 16 // bit signed
-`define NUMBER_OF_VARIABLES_WITH_INSIDE_CONSTRAINT 1
-module TopModule;
-    reg [CLAUSE_BIT_WIDTH-1:0] formula [NUMBER_OF_CLAUSES]
-    reg [(NUMBER_OF_BOOLEAN_VARIABL+(NUMBER_OF_INTEGER_VARIABLES*BIT_WIDTH_OF_INTEGER_VARIABLE))-1:0] initial_assigmnets 
-    // sizes buffer
-    Solver solver(formula[1],pls0,temperature,initial_assigmnets);
+parameter NUMBER_OF_CLAUSES = 3; // does not include "inside" constraint
+parameter NUMBER_OF_INTEGER_VARIABLES = 2;
+parameter NUMBER_OF_BOOLEAN_VARIABLES = 2;
+parameter BIT_WIDTH_OF_INTEGER_VARIABLE = 16;
+parameter BIT_WIDTH_OF_COEFFICIENTS = 16; // bit signed
+parameter CLAUSE_BIT_WIDTH = (( NUMBER_OF_INTEGER_VARIABLES * BIT_WIDTH_OF_COEFFICIENTS )+(2 * NUMBER_OF_BOOLEAN_VARIABLES )) ; //(2*NUMBER_OF_BOOLEAN_VARIABLES) 1 bit to indicate whether is exist or not and the other bit is a the constraint (x or !x)
+parameter NUMBER_OF_VARIABLES_WITH_INSIDE_CONSTRAINT = 1;
 
+// GENERAL NOTE -> any probability will deal with it as an integer from 0->100 instead of 0 ->1
+
+module TopModule(
+input [7:0] in_pls0, // probability of local search move
+input [7:0] in_temperature // Hyper Parameter for tuning 
+);
+    reg [CLAUSE_BIT_WIDTH - 1:0] formula [NUMBER_OF_CLAUSES : 0 ] ; // contains all the coeffiecient 
+    reg [(NUMBER_OF_BOOLEAN_VARIABLES+(NUMBER_OF_INTEGER_VARIABLES*BIT_WIDTH_OF_INTEGER_VARIABLE))-1:0] initial_assigmnets;
+ 
+    // sizes buffer will determine the parameter (constants values later)
+    
+    // read the formula from the text file generated from the pre-processing 
+    
+    // control unit here to pass different clauses for the solver instances at every clock cycle
+         
+    Solver solver(formula[1],in_pls0,in_temperature,initial_assigmnets);
+    
 endmodule
