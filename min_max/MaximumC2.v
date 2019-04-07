729b0013b8ab19ec5e7d89b9356b7eda865ff2fe
manar <manar199631@gmail>
2019-04-07 18:16:04 +0200

    manar---modifying min_max

--- min_max/MaximumC2.v
+++ min_max/MaximumC2.v
@@ -1,7 +1,7 @@
-`timescale 1ns / 1ps
+
 //This module will pass the maximum of two signed numbers
 
-module MaximumC1(
+module MaximumC2(
 input wire signed [7:0]first_number,
 input wire first_number_activation,//tells the module if it should consider this number to compare with the other one
 input wire first_number_sign,
@@ -12,11 +12,11 @@ input wire second_number_activation,
 input wire second_number_sign,
 
 output wire signed [7:0] maximum,
-output wire	minimum_activation //tells the socond layr if this maximum valid or not
+output wire	maximum_activation //tells the socond layr if this maximum valid or not
 );
-assign minimum =((first_number>=second_number)?
+assign maximum =((first_number>=second_number)?
                     ((first_number_activation==1&&first_number_sign==0)?(first_number):(second_number)):
-                    ((second_number_activation==1&&first_number_sign==0)?(second_number):(first_number)));
+                    ((second_number_activation==1&&second_number_sign==0)?(second_number):(first_number)));
                     
 assign maximum_activation = (first_number_activation&(~first_number_sign))|(second_number_activation&(~second_number_sign));
                    
