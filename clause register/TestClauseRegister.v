c83f2395f6f5aa7dbfa90bdbc9f1f28204b3eaaa
younanNagy <younan.nagy2@gmail.com>
2019-04-30 01:30:48 +0200

    Younan adding clause register

--- clause register/TestClauseRegister.v
+++ clause register/TestClauseRegister.v
@@ -1,61 +1,62 @@
-
-`include "TopModuleHeaders.vh"
-
+`timescale 1ns / 1ps
 module TestClauseRegister(
     );
+    parameter BIT_WIDTH_OF_INTEGER_VARIABLE=2;
+    parameter NUMBER_OF_INTEGER_VARIABLES=2;
+    parameter MODULE_IDENTIFIER=1;
     
-    
-             reg [(`BIT_WIDTH_OF_INTEGER_VARIABLE * `NUMBER_OF_INTEGER_VARIABLES) -1 : 0 ] in_clause_coefficients;
-             reg in_write_enable;
-             reg in_clk;
-             wire [(`BIT_WIDTH_OF_INTEGER_VARIABLE * `NUMBER_OF_INTEGER_VARIABLES) -1 : 0  ] out_clause_coefficients;
-          
-    
-    
-    
-    
+    reg [(BIT_WIDTH_OF_INTEGER_VARIABLE * NUMBER_OF_INTEGER_VARIABLES) -1 : 0 ] in_clause_coefficients;
+    reg in_calause_index;
+    reg in_reset;
+    reg in_clk;
+    wire [(BIT_WIDTH_OF_INTEGER_VARIABLE * NUMBER_OF_INTEGER_VARIABLES) -1 : 0  ] out_clause_coefficients;
 initial
  begin
 in_clk = 0;
+in_reset=1;
+in_calause_index = 1;
 forever
- #50 in_clk = ~ in_clk;
+ #50
+ in_clk = ~ in_clk;
 end
 
 initial
 begin
-           
-in_write_enable = 0;
+ $monitor("Time=%t ++ the input coeffs line=%d  ++ clock=%b ++ the output coeffs line = %d  reset=%b",$realtime,in_clause_coefficients, in_clk, out_clause_coefficients,in_reset);           
+//in_calause_index = 1;
+#100
+in_reset=0;
+in_clause_coefficients = 1;
+
 #100
 
-in_write_enable = 1;
+//in_calause_index = 1;
 in_clause_coefficients = 7;
 #100
 
-in_write_enable = 0;
+//in_calause_index = 1;
 in_clause_coefficients = 8;
 #100
 
 
 
-in_write_enable = 1;
+in_calause_index = 0;
 in_clause_coefficients = 9;
 #100;
 
 
 end
-
+/*
 initial
 begin
  $display("output of testing CluaseRegister : ");
- $monitor(" the output coeffs line = %d",  out_clause_coefficients);
- end
-          
-    
-    
-    
-    ClauseRegister uut(
+
+ end*/
+    ClauseRegister
+    uut(
          .in_clause_coefficients(in_clause_coefficients),
-         .in_write_enable(in_write_enable),
+         .in_calause_index(in_calause_index),
+         .in_reset(in_reset),
          .in_clk(in_clk),
          .out_clause_coefficients(out_clause_coefficients)
         );
--- clause register/clause_register.v
+++ clause register/clause_register.v
@@ -1,7 +1,4 @@
-
-`include "TopModuleHeaders.vh"
-
-
+`timescale 1ns / 1ps
 // Inputs
 // 1) Clause coeffs
 // 2) Write enable 
@@ -9,23 +6,36 @@
 
 // Outputs
 // New Clause coeff
-
-
-module ClauseRegister(
+module ClauseRegister
+#(
+    parameter MAXIMUM_BIT_WIDTH_OF_COEFFICIENT=2,
+    parameter NUMBER_OF_INTEGER_VARIABLES=2,
+    parameter MODULE_IDENTIFIER=1,
+    parameter MAX_BIT_WIDTH_OF_CLAUSES_INDEX=1
+)(
      
-     input [(`BIT_WIDTH_OF_INTEGER_VARIABLE * `NUMBER_OF_INTEGER_VARIABLES) -1 : 0 ] in_clause_coefficients,
-     input in_write_enable,
+     input [(MAXIMUM_BIT_WIDTH_OF_COEFFICIENT * (NUMBER_OF_INTEGER_VARIABLES+1//for the bias
+     )) -1 : 0 ] in_clause_coefficients,
+     input [MAX_BIT_WIDTH_OF_CLAUSES_INDEX-1:0]in_clause_index,
+     input in_reset,
      input in_clk,
-     output reg [(`BIT_WIDTH_OF_INTEGER_VARIABLE * `NUMBER_OF_INTEGER_VARIABLES) -1 : 0  ] out_clause_coefficients
+                                                                       
+     output reg [(MAXIMUM_BIT_WIDTH_OF_COEFFICIENT * (NUMBER_OF_INTEGER_VARIABLES+1//for the bias
+     )) -1 : 0  ] out_clause_coefficients
   
     );
     
         
     always @(posedge in_clk)
     begin
-       
-       if(in_write_enable)
-        out_clause_coefficients = in_clause_coefficients;       
+       if(in_reset)
+        out_clause_coefficients <= {(MAXIMUM_BIT_WIDTH_OF_COEFFICIENT * NUMBER_OF_INTEGER_VARIABLES)  {1'b0}};
+        else begin
+           if(in_clause_index==MODULE_IDENTIFIER)
+            out_clause_coefficients <= in_clause_coefficients;
+           else
+            out_clause_coefficients<=out_clause_coefficients;
+        end          
     end
  
 endmodule
