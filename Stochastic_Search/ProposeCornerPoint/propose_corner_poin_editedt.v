37200387722c64e70a305c0fe3b6579f57c5f642
manar abdelrahman <manar199631@gmail.com>
2019-05-10 15:53:08 -0700

    manar---adding the stochastic search folders

---
+++ Stochastic_Search/ProposeCornerPoint/propose_corner_poin_editedt.v
@@ -0,0 +1,173 @@
+`timescale 1ns / 1ps
+
+module propose_corner_point_edited
+#(
+
+parameter MAXIMUM_BIT_WIDTH_OF_COEFFICIENT = 8,
+parameter MAXIMUM_BIT_WIDTH_OF_VARIABLE_INDEX = 2,
+parameter MAX_BIT_WIDTH_OF_INTEGER_VARIABLE=4,
+parameter MAX_BIT_WIDTH_OF_CLAUSES_INDEX=3
+)
+(
+input in_clk,
+input in_reset,
+// for clause register setup
+input [((2**MAXIMUM_BIT_WIDTH_OF_VARIABLE_INDEX)+1//for the bias
+)*MAXIMUM_BIT_WIDTH_OF_COEFFICIENT-1:0]in_clause_coefficients,//coefficients of one clause only
+
+input [MAX_BIT_WIDTH_OF_CLAUSES_INDEX-1:0] in_clause_index,
+
+input [MAX_BIT_WIDTH_OF_INTEGER_VARIABLE*(2**MAXIMUM_BIT_WIDTH_OF_VARIABLE_INDEX)-1:0]in_assignment_before_move,
+input [MAXIMUM_BIT_WIDTH_OF_VARIABLE_INDEX-1:0] in_variable_to_be_unchanced_index,
+
+input [(2**MAX_BIT_WIDTH_OF_CLAUSES_INDEX)-1:0]in_reduce_enable,
+
+//this should be [MAX_BIT_WIDTH_OF_INTEGER_VARIABLE-1:0] but the new assignment here is one of the biases
+//so we have to make MAXIMUM_BIT_WIDTH_OF_COEFFICIENT=MAX_BIT_WIDTH_OF_INTEGER_VARIABLE
+output reg signed [MAXIMUM_BIT_WIDTH_OF_COEFFICIENT-1:0]new_assignmet_for_the_choosen_variable
+);
+wire [((2**MAXIMUM_BIT_WIDTH_OF_VARIABLE_INDEX)+1//for the bias
+)*MAXIMUM_BIT_WIDTH_OF_COEFFICIENT-1:0]clauses_coefficients[0:(2**MAX_BIT_WIDTH_OF_CLAUSES_INDEX)-1];
+
+
+wire signed[MAXIMUM_BIT_WIDTH_OF_COEFFICIENT-1:0]reduce_out_biases[0:((2**MAX_BIT_WIDTH_OF_CLAUSES_INDEX)-1)-1];
+wire reduce_out_activations[0:((2**MAX_BIT_WIDTH_OF_CLAUSES_INDEX)-1)-1];
+wire reduce_out_sign[0:((2**MAX_BIT_WIDTH_OF_CLAUSES_INDEX)-1)-1];//the clause is y+c>0 or y+c<0
+
+wire signed[MAXIMUM_BIT_WIDTH_OF_COEFFICIENT-1:0]tree_comparator_biases_for_c1[0:((2**MAX_BIT_WIDTH_OF_CLAUSES_INDEX)-1)-1];
+wire tree_comparator_activations_for_c1[0:((2**MAX_BIT_WIDTH_OF_CLAUSES_INDEX)-1)-1]; 
+
+
+wire signed[MAXIMUM_BIT_WIDTH_OF_COEFFICIENT-1:0]tree_comparator_biases_for_c2[0:((2**MAX_BIT_WIDTH_OF_CLAUSES_INDEX)-1)-1];
+wire tree_comparator_activations_for_c2[0:((2**MAX_BIT_WIDTH_OF_CLAUSES_INDEX)-1)-1]; 
+
+generate 
+genvar i,j;
+for(i=0;i<(2**MAX_BIT_WIDTH_OF_CLAUSES_INDEX);i=i+1)begin:generate_reduce
+
+ClauseRegister_IntegerLiteral
+#( 
+ .MAXIMUM_BIT_WIDTH_OF_INTEGER_COEFFICIENT(MAXIMUM_BIT_WIDTH_OF_COEFFICIENT),
+ .NUMBER_OF_INTEGER_VARIABLES(2**MAXIMUM_BIT_WIDTH_OF_VARIABLE_INDEX),
+ .MODULE_IDENTIFIER(i),
+ .MAX_BIT_WIDTH_OF_CLAUSES_INDEX(MAX_BIT_WIDTH_OF_CLAUSES_INDEX)
+)
+    clause_register_integer_literal(
+         .in_clause_coefficients(in_clause_coefficients),
+         .in_clause_index(in_clause_index),
+         .in_reset(in_reset),
+         .in_clk(in_clk),
+         .out_clause_coefficients(clauses_coefficients[i])
+        );
+
+ ReduceClause
+ #(
+    .MAXIMUM_BIT_WIDTH_OF_COEFFICIENT(MAXIMUM_BIT_WIDTH_OF_COEFFICIENT),
+    .MAXIMUM_BIT_WIDTH_OF_VARIABLE_INDEX(MAXIMUM_BIT_WIDTH_OF_VARIABLE_INDEX)
+  )
+ reduce_clause 
+(
+    .in_coefficients(clauses_coefficients[i]),
+    .in_current_assignment(in_assignment_before_move),
+    .in_variable_to_be_unchanged_index(in_variable_to_be_unchanced_index),
+    .in_clk(in_clk),
+    .in_enable(in_reduce_enable[i]),
+    .in_reset(in_reset),
+    .out_Bias(reduce_out_biases[i]),
+    .out_Variable_to_be_unchanged_sign(reduce_out_sign[i]),
+    .out_Active(reduce_out_activations[i])
+    ); 
+end
+
+for(i=0;i<(2**MAX_BIT_WIDTH_OF_CLAUSES_INDEX);i=i+2)begin:first_level_of_the_tree_comparator
+MaximumC1
+    #( .NUMBER_SIZE(MAXIMUM_BIT_WIDTH_OF_COEFFICIENT) ) 
+    
+maxc1(
+.first_number(reduce_out_biases[i]),
+.first_number_activation(reduce_out_activations[i]),
+.first_number_sign(reduce_out_sign[i]),
+
+.second_number(reduce_out_biases[i+1]),
+.second_number_activation(reduce_out_activations[i+1]),
+.second_number_sign(reduce_out_sign[i+1]),
+
+.maximum(tree_comparator_biases_for_c1[i/2]),
+.maximum_activation(tree_comparator_activations_for_c1[i/2])
+);
+
+MaximumC2
+    #( .NUMBER_SIZE(MAXIMUM_BIT_WIDTH_OF_COEFFICIENT) ) 
+    
+maxc2(
+.first_number(reduce_out_biases[i]),
+.first_number_activation(reduce_out_activations[i]),
+.first_number_sign(reduce_out_sign[i]),
+
+.second_number(reduce_out_biases[i+1]),
+.second_number_activation(reduce_out_activations[i+1]),
+.second_number_sign(reduce_out_sign[i+1]),
+
+.maximum(tree_comparator_biases_for_c2[i/2]),
+.maximum_activation(tree_comparator_activations_for_c2[i/2])
+);
+end  
+                                //  *
+                               //  ***   
+//total number of nodes in a tree ***** = 5+4 = 2*numberof nodes in last level -1 
+//number of nodes in the last level of our tree = number of reduce blocks /2
+for (i=0;i<2**(MAX_BIT_WIDTH_OF_CLAUSES_INDEX)-1;i=(i/2)+2**(MAX_BIT_WIDTH_OF_CLAUSES_INDEX-1))begin :generate_secod_level_in_the_tree_for_c1
+         for(j=i;j<(i/2)+2**(MAX_BIT_WIDTH_OF_CLAUSES_INDEX-1)-1;j=j+2)begin : generate_ith_level
+             Minimum_C1_with_active_signal_only 
+             #( .NUMBER_SIZE(MAXIMUM_BIT_WIDTH_OF_COEFFICIENT))
+             min_c1(
+                 .first_number(tree_comparator_biases_for_c1[j]),
+                 .first_number_activation(tree_comparator_activations_for_c1[j]),
+                
+                 
+                 .second_number(tree_comparator_biases_for_c1[j+1]),
+                 .second_number_activation(tree_comparator_activations_for_c1[j+1]),
+                 
+                 
+                 .maximum(tree_comparator_biases_for_c1[(i/2)+2**(MAX_BIT_WIDTH_OF_CLAUSES_INDEX-1)+((j-i)/2)]),
+                 .maximum_activation(tree_comparator_activations_for_c1[(i/2)+2**(MAX_BIT_WIDTH_OF_CLAUSES_INDEX-1)+((j-i)/2)])
+                 );
+            end
+end
+
+// for loops for c2 tree comparator
+
+for (i=0;i<2**(MAX_BIT_WIDTH_OF_CLAUSES_INDEX)-1;i=(i/2)+2**(MAX_BIT_WIDTH_OF_CLAUSES_INDEX-1))begin :generate_secod_level_in_the_tree_for_c2
+         for(j=i;j<(i/2)+2**(MAX_BIT_WIDTH_OF_CLAUSES_INDEX-1)-1;j=j+2)begin : generate_ith_level
+             Maximum_C2_with_active_signal_only 
+             #( .NUMBER_SIZE(MAXIMUM_BIT_WIDTH_OF_COEFFICIENT))
+             max_c2(
+                 .first_number(tree_comparator_biases_for_c2[j]),
+                 .first_number_activation(tree_comparator_activations_for_c2[j]),
+                
+                 
+                 .second_number(tree_comparator_biases_for_c2[j+1]),
+                 .second_number_activation(tree_comparator_activations_for_c2[j+1]),
+                 
+                 
+                 .maximum(tree_comparator_biases_for_c2[(i/2)+2**(MAX_BIT_WIDTH_OF_CLAUSES_INDEX-1)+((j-i)/2)]),
+                 .maximum_activation(tree_comparator_activations_for_c2[(i/2)+2**(MAX_BIT_WIDTH_OF_CLAUSES_INDEX-1)+((j-i)/2)])
+                 );
+            end
+end
+
+
+endgenerate 
+
+
+always@(posedge(in_clk))begin
+//if state = propose 
+// this state comes from the control unit
+if(tree_comparator_activations_for_c1[((2**MAX_BIT_WIDTH_OF_CLAUSES_INDEX)-1)-1])
+new_assignmet_for_the_choosen_variable<=tree_comparator_biases_for_c1[((2**MAX_BIT_WIDTH_OF_CLAUSES_INDEX)-1)-1];
+else
+new_assignmet_for_the_choosen_variable<=tree_comparator_biases_for_c2[((2**MAX_BIT_WIDTH_OF_CLAUSES_INDEX)-1)-1];
+end
+
+
+endmodule
---
+++ Stochastic_Search/compute_gain/computeGain_tb.v
@@ -0,0 +1,118 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/07/2019 11:56:01 PM
+// Design Name: 
+// Module Name: computeGain_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module computeGain_tb(
+    );
+    parameter MAXIMUM_BIT_WIDTH_OF_INTEGER_COEFFICIENT = 4;//gives us the maximum bitwidth of coefficients in integer literal
+    parameter MAXIMUM_BIT_WIDTH_OF_BOOLEAN_COEFFICIENT = 2;//gives us the maximum bitwidth of coefficients in boolean literal(it always equals 2)
+    parameter MAXIMUM_BIT_WIDTH_OF_INTEGER_VARIABLE_INDEX = 1;//gives us the maximum number of integer variables in clause
+    parameter MAXIMUM_BIT_WIDTH_OF_BOOLEAN_VARIABLE_INDEX = 1;//gives us the maximum number of boolean variables in clause
+    parameter MAXIMUM_BIT_WIDTH_OF_INTEGER_VARIABLE=4;//gives us the maximum bitwidth of the value assigned to an integer variable
+    parameter MAXIMUM_BIT_WIDTH_OF_BOOLEAN_VARIABLE=1;//gives us the maximum bitwidth of the value assigned to an integer variable
+    parameter MAXIMUM_BIT_WIDTH_OF_CLAUSES_INDEX = 2;//gives us the maximum number of clauses
+
+    reg in_clk;
+    reg in_reset;
+    reg in_enable;
+    // for clause register setup
+    reg [((2**MAXIMUM_BIT_WIDTH_OF_INTEGER_VARIABLE_INDEX)+1//for the bias
+    )*MAXIMUM_BIT_WIDTH_OF_INTEGER_COEFFICIENT-1:0]in_clause_coefficients_integer;//coefficients of integer literal for one clause only 
+    reg [(MAXIMUM_BIT_WIDTH_OF_BOOLEAN_COEFFICIENT*(2**MAXIMUM_BIT_WIDTH_OF_BOOLEAN_VARIABLE_INDEX))-1 : 0 ] in_clause_coefficients_boolean;//coefficients of boolean literals for one clause only
+    reg [MAXIMUM_BIT_WIDTH_OF_CLAUSES_INDEX-1:0] in_clause_index;//the number of the clause
+    
+    //the assignment to check
+    reg [MAXIMUM_BIT_WIDTH_OF_INTEGER_VARIABLE*(2**MAXIMUM_BIT_WIDTH_OF_INTEGER_VARIABLE_INDEX)-1:0]in_integer_assignment_after_move;
+    reg [MAXIMUM_BIT_WIDTH_OF_BOOLEAN_VARIABLE*(2**MAXIMUM_BIT_WIDTH_OF_BOOLEAN_VARIABLE_INDEX)-1:0]in_boolean_assignment_after_move;
+    wire [MAXIMUM_BIT_WIDTH_OF_CLAUSES_INDEX-1:0] out_number_of_satisfied_clauses;
+    
+    
+   ComputeGain
+    #(
+       MAXIMUM_BIT_WIDTH_OF_INTEGER_COEFFICIENT ,
+       MAXIMUM_BIT_WIDTH_OF_BOOLEAN_COEFFICIENT ,
+       MAXIMUM_BIT_WIDTH_OF_INTEGER_VARIABLE_INDEX,
+       MAXIMUM_BIT_WIDTH_OF_BOOLEAN_VARIABLE_INDEX,
+       MAXIMUM_BIT_WIDTH_OF_INTEGER_VARIABLE,
+       MAXIMUM_BIT_WIDTH_OF_BOOLEAN_VARIABLE,
+       MAXIMUM_BIT_WIDTH_OF_CLAUSES_INDEX
+    )
+    computeGainTB
+    ( in_clk,
+     in_reset,
+     in_enable,
+     in_clause_coefficients_integer,
+     in_clause_coefficients_boolean,
+     in_clause_index,
+     in_integer_assignment_after_move,
+     in_boolean_assignment_after_move,
+    out_number_of_satisfied_clauses  
+     );
+    
+    
+    
+    initial
+    begin
+    in_clk=0;
+    end
+    
+    
+    initial
+    begin
+    //at first set up the module(set the formula) 
+    ////
+    in_reset = 0;
+    in_integer_assignment_after_move=12'h111;
+    in_boolean_assignment_after_move=2'b11;
+    in_clause_coefficients_integer =8'hff;
+    in_clause_coefficients_boolean=4'h0;
+    in_clause_index=0;
+    #200;
+    in_clause_coefficients_integer =8'h11;
+    in_clause_coefficients_boolean=4'hf;
+    in_clause_index=1;
+    #200;
+    in_clause_coefficients_integer =8'hf1;
+    in_clause_coefficients_boolean=4'hf;
+    in_clause_index=2;    
+    #200;
+    in_clause_coefficients_integer =8'h11;
+    in_clause_coefficients_boolean=4'h0;
+    in_clause_index=3;
+    /////
+    //after seting up the module ,enable it  
+    #200;
+    in_enable=1;
+      
+    //#400;
+    //in_reset=1; 
+    end
+    
+    
+    always
+    begin
+    #100;
+    in_clk = ~in_clk;
+    end
+    
+    
+    
+endmodule
