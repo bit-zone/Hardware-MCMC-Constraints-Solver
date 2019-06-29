17064f63c77516d4639720be5b227b67cd8590ff
Youssef Ahmed <yousseffahmed@hotmail.com>
2019-06-29 12:45:07 +0200

    Add files via upload

---
+++ UnsatisfiedClauseAtRandom/compare_two_checker.v
@@ -0,0 +1,89 @@
+// inputs
+// 1- two indeces 
+// 2- two flags for unsatisfied 
+// 3- one control signal if 0 choose the first , if 1 choose the second
+
+// ----------------------------------
+
+// outputs
+// the index of the clause 
+// is it satisfied or not
+
+
+module CompareTwoChecker
+#(
+parameter MAXIMUM_BIT_WIDTH_OF_CLAUSES_INDEX = 2//gives us the maximum number of clauses
+)
+
+(
+input [MAXIMUM_BIT_WIDTH_OF_CLAUSES_INDEX-1:0] in_clause_1_index,
+input [MAXIMUM_BIT_WIDTH_OF_CLAUSES_INDEX-1:0] in_clause_2_index,
+input in_clause_1_satisfied,
+input in_clause_2_satisfied,
+input in_setting,
+output [MAXIMUM_BIT_WIDTH_OF_CLAUSES_INDEX-1:0] out_clause_index,
+output out_clause_satisfied
+);
+
+reg  [MAXIMUM_BIT_WIDTH_OF_CLAUSES_INDEX-1:0] index;
+reg is_satisfied;
+
+always @ *
+begin
+
+    // if the first clause is not satisfied and the second is satisfied propagate the first
+    if(in_clause_1_satisfied == 0 && in_clause_2_satisfied == 1)
+    begin
+    index = in_clause_1_index;
+    is_satisfied = 0;
+    end
+
+
+    // if the first clause is satisfied and the second is not propagate the second
+    if(in_clause_1_satisfied == 1 && in_clause_2_satisfied == 0)
+    begin
+    index = in_clause_2_index;
+    is_satisfied = 0;
+    end
+    
+
+    // if the first clause is satisfied and the second is not propagate the second
+    if(in_clause_1_satisfied == 1 && in_clause_2_satisfied == 0)
+    begin
+    index = in_clause_2_index;
+    is_satisfied = 0;
+    end
+
+
+    // if booth not satisfied , choose the one from the control signal setting
+    if(in_clause_1_satisfied == 0 && in_clause_2_satisfied == 0)
+    begin
+    
+        if(in_setting == 0)
+            index = in_clause_1_index;
+        else
+             index = in_clause_2_index;
+            
+    is_satisfied = 0;
+    end
+       
+
+        // if booth satisfied , choose the one from the control signal setting
+    if(in_clause_1_satisfied == 1 && in_clause_2_satisfied == 1)
+    begin
+    
+        if(in_setting == 0)
+            index = in_clause_1_index;
+        else
+             index = in_clause_2_index;
+            
+    is_satisfied = 1;
+    end
+
+ end
+ 
+
+ assign out_clause_index = index;
+ assign out_clause_satisfied = is_satisfied; 
+ 
+endmodule
---
+++ UnsatisfiedClauseAtRandom/test_check_two.v
@@ -0,0 +1,84 @@
+
+module TestCheckTwo
+#(
+parameter MAXIMUM_BIT_WIDTH_OF_CLAUSES_INDEX = 1//gives us the maximum number of clauses
+);
+ 
+ // Inputs
+ reg [MAXIMUM_BIT_WIDTH_OF_CLAUSES_INDEX-1:0] in_clause_1_index;
+ reg [MAXIMUM_BIT_WIDTH_OF_CLAUSES_INDEX-1:0] in_clause_2_index;
+ reg in_clause_1_satisfied;
+ reg in_clause_2_satisfied;
+ reg in_setting;
+ 
+ // Outputs
+  wire [MAXIMUM_BIT_WIDTH_OF_CLAUSES_INDEX-1:0] out_clause_index;
+  wire out_clause_satisfied;
+
+
+initial begin  
+ #10
+ in_setting = 0;
+ in_clause_1_index = 0;
+ in_clause_2_index = 1;
+ in_clause_1_satisfied = 0;
+ in_clause_2_satisfied = 1;
+
+ #10
+ in_setting = 0;
+ in_clause_1_index = 0;
+ in_clause_2_index = 1;
+ in_clause_1_satisfied = 1;
+ in_clause_2_satisfied = 0;
+
+
+ #10
+ in_setting = 0;
+ in_clause_1_index = 0;
+ in_clause_2_index = 1;
+ in_clause_1_satisfied = 1;
+ in_clause_2_satisfied = 1;
+
+
+ #10
+ in_setting = 0;
+ in_clause_1_index = 0;
+ in_clause_2_index = 1;
+ in_clause_1_satisfied = 0;
+ in_clause_2_satisfied = 0;
+
+
+
+
+
+
+
+
+
+
+
+ 
+end
+ 
+
+
+
+ initial begin
+ $display("output of testing..");
+ $monitor(" the index is %d",  out_clause_index);
+ $monitor(" the clause is satisfied : %d",  out_clause_satisfied);
+ end     
+
+
+ // Instantiate the Unit Under Test (UUT)
+ CompareTwoChecker  uut (
+  .in_clause_1_index(in_clause_1_index), 
+  .in_clause_2_index(in_clause_2_index),
+  .in_clause_1_satisfied(in_clause_1_satisfied),
+  .in_clause_2_satisfied(in_clause_2_satisfied),
+  .in_setting(in_setting),
+  .out_clause_index(out_clause_index),
+  .out_clause_satisfied(out_clause_satisfied)
+ );
+ 
+endmodule  
